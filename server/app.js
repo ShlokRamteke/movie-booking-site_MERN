3ab07137277c38659f15dfff0e27a226e3568cc6
Shlok Ramteke <shlok.ramteke24@gmail.com>
2023-08-19 10:04:34 +0530

    databse connected

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
-console.log("Hi");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = 5000;
+const app = express();
+const db = process.env.DATABASE;
+
+mongoose
+  .connect(db)
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`Connected to Database and server is running at ${port}`);
+    })
+  )
+  .catch((error) => console.log(error));
